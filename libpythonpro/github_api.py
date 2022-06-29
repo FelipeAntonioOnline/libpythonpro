7115887405036ae947f1f31a508d2e9a55b4f390
Felipe Antonio <felipe.antonio.online@proton.me>
2022-06-29 11:08:34 -0300

    Add github_api module.

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def busca_avatar(usuario):
+    """
+    Encontra e retorna a url do avatar do usuário do github."""
+
+    url = f"https://api.github.com/users/{usuario}"
+    resp = requests.get(url)
+    return resp.json()["avatar_url"]
+
+
+if __name__ == "__main__":
+    print(busca_avatar("felipeantonioonline"))
