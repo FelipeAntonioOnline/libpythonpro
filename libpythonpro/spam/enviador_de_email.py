8e37d62a653cb8afe042d9705713687a0a8e8a57
Felipe Antonio <felipe.antonio.online@proton.me>
2022-07-02 16:46:47 -0300

    Update enviador_de_email
    Method now return remetente.

--- libpythonpro/spam/enviador_de_email.py
+++ libpythonpro/spam/enviador_de_email.py
@@ -1,3 +1,3 @@
 class Enviador:
     def enviar(self, remetente, destinatario, assunto, corpo):
-        return "felipe.antonio.online@pronton.me"
+        return remetente
---
+++ libpythonpro/tests/test_spam.py
@@ -0,0 +1,21 @@
+import pytest
+from libpythonpro.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    "remetente", ["felipe.antonio.online@proton.me", "foo@bar.com"]
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        "felipe23antonio@gmail.com",
+        "Curso Python Tools",
+        "Teste para verificar a funcionalidade de Enviado.enviar()",
+    )
+    assert remetente in resultado
