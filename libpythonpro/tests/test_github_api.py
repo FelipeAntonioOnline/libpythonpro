8b2bcb7c5c84175f04943b82869f147f3657e47a
Felipe Antonio <felipe.antonio.online@proton.me>
2022-07-06 17:59:12 -0300

    Fix
    Refactor avatar_url to work with pytest-mock.

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -1,11 +1,10 @@
 from unittest.mock import Mock
-
 import pytest
 from libpythonpro import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = "https://avatars.githubusercontent.com/u/mock"
     resp_mock.json.return_value = {
@@ -13,10 +12,9 @@ def avatar_url():
         "id": 100208168,
         "avatar_url": url,
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch("libpythonpro.github_api.requests.get")
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_busca_avatar(avatar_url):
