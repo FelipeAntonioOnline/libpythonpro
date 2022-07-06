a9fdc9903df53aa5a5fb863a9e48b5649ccbdeef
Felipe Antonio <felipe.antonio.online@proton.me>
2022-07-06 15:04:11 -0300

    Fix/Feat
    Add EnviadorMock to use in tests instead of Enviador.
    
    Refactor the code to reflect the change.

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -4,6 +4,17 @@ from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
 
 
+class EnviadorMock(Enviador):
+    def __init__(self):
+        super().__init__()
+        self.qtde_emails_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtde_emails_enviados += 1
+
+
 @pytest.mark.parametrize(
     "usuarios",
     [
@@ -19,7 +30,7 @@ from libpythonpro.spam.modelos import Usuario
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         "felipe.antonio.online@proton.me",
@@ -27,3 +38,21 @@ def test_qtde_de_spam(sessao, usuarios):
         "Veja o que estamos aprendendo.",
     )
     assert len(usuarios) == enviador.qtde_emails_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome="Felipe", email="felipe.antonio.online@proton.me")
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        "foo@proton.me",
+        "Curso Python Pro",
+        "Veja o que estamos aprendendo.",
+    )
+    assert enviador.parametros_de_envio == (
+        "foo@proton.me",
+        "felipe.antonio.online@proton.me",
+        "Curso Python Pro",
+        "Veja o que estamos aprendendo.",
+    )
