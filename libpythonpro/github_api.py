a8f4ec9fdeea3b5065809827fd6c4bdc95e0e13a
Felipe Antonio <felipe.antonio.online@proton.me>
2022-07-06 16:45:36 -0300

    Fix
    Remove internal test.

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -8,7 +8,3 @@ def busca_avatar(usuario):
     url = f"https://api.github.com/users/{usuario}"
     resp = requests.get(url)
     return resp.json()["avatar_url"]
-
-
-if __name__ == "__main__":
-    print(busca_avatar("felipeantonioonline"))
