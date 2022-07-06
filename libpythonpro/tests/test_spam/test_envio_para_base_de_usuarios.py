d74395d6ede283d914484c94754648e94403b11f
Felipe Antonio <felipe.antonio.online@proton.me>
2022-07-06 15:13:19 -0300

    Fix
    Remove class EnviadorMock
    
    Refactor tests to use lib mock.

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,18 +1,7 @@
+from unittest.mock import Mock
 import pytest
-from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 from libpythonpro.spam.modelos import Usuario
-
-
-class EnviadorMock(Enviador):
-    def __init__(self):
-        super().__init__()
-        self.qtde_emails_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtde_emails_enviados += 1
 
 
 @pytest.mark.parametrize(
@@ -30,27 +19,27 @@ class EnviadorMock(Enviador):
 def test_qtde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         "felipe.antonio.online@proton.me",
         "Curso Python Pro",
         "Veja o que estamos aprendendo.",
     )
-    assert len(usuarios) == enviador.qtde_emails_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome="Felipe", email="felipe.antonio.online@proton.me")
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         "foo@proton.me",
         "Curso Python Pro",
         "Veja o que estamos aprendendo.",
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with == (
         "foo@proton.me",
         "felipe.antonio.online@proton.me",
         "Curso Python Pro",
